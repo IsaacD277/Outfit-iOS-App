e7f8c39da766f24b953bb8543fa109d6296f66bc
Isaac D2 <idarr08@gmail.com>
2025-03-10 17:47:46 -0400

    Initial Commit

---
+++ Outfitter/OutfitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitterApp.swift
+//  Outfitter
+//
+//  Created by Isaac D2 on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct OutfitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
